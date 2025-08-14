49650ba1961029529b83fa564f114c9a36ae2293
phumptom <phumptom.fun@gmail.com>
2025-08-14 12:46:07 +0200

    Delete modules directory

--- modules/ui/hud.js
+++
@@ -1,4 +0,0 @@
-// Step 09 — HUD & inventory helpers extracted from game.js
-export function installHUD(game) {
-  // TODO: no methods found to install
-}
--- modules/ui/progress.js
+++
@@ -1,4 +0,0 @@
-// Step 09 — UI progress & counters extracted from game.js
-export function installUIProgress(game) {
-  // TODO: no methods found to install
-}
